7ba9be85491407af4105e4d778206949747d6868
Erik Shaver <75393241+ki-krpto@users.noreply.github.com>
2025-11-14 22:36:34 -0800

    Hero Banner!!!

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -13,10 +13,20 @@ function Blog() {
   )
 }
 
-function About() {
+function Home() {
   return (
     <>
-      <h1>About</h1>
+    <div className="container circle-wrapper">
+      <img src={Logo} alt="Logo" className="Hero-Logo circular-image"/>
+    </div>
+    <div className="container">
+      <div className="hero-text">
+        <h1 className="hero-header">Hi, I'm Erik Shaver</h1>
+        <p className="hero-subheader">Web Developer & Aspiring Pentester</p>
+        <button className="btn btn-primary contact-button">Contact</button>
+        <button className="btn btn-primary projects-button">Projects</button>
+      </div>
+    </div>
     </>
   )
 }
@@ -24,20 +34,20 @@ function About() {
 function App() {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary navigation-bar">
+      <Navbar expand="lg" className="bg-body-tertiary navigation-bar w-100">
         <div className="container-fluid">
           <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
             <img
             src={Logo}
             alt="Logo"
-            className="logo"
+            className="logo me-3"
             />
             <span>Erik Shaver</span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/" className="nav-link">About</Nav.Link>
+              <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
               <Nav.Link as={Link} to="/projects" className="nav-link">Projects</Nav.Link>
               <Nav.Link as={Link} to="/blog" className="nav-link">Blog</Nav.Link>
               <NavDropdown title="Resources" id="basic-nav-dropdown">
@@ -52,8 +62,12 @@ function App() {
       </Navbar>
       <Routes>
         <Route path="/blog" element={<Blog/>} />
-        <Route path="/" element={<About/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
+      <footer className="footer">
+        <p>© 2025 Erik Shaver. All rights reserved.</p>
+      </footer>
     </>
   );
 }
--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -5,11 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import './App.css'
 import App from './App.jsx'
+import {HashRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
