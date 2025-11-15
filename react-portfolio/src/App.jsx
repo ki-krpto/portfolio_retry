c1e68344a5b75f0062868177cd81ff07ef48fb62
Erik Shaver <75393241+ki-krpto@users.noreply.github.com>
2025-11-14 19:43:47 -0800

    Added Navbar FINALLY to site

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,30 +1,61 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link, Route, Routes } from 'react-router-dom';
+import Logo from './assets/Logo.webp';
 
-function BasicExample() {
+
+function Blog() {
+  return (
+    <>
+      <h1>Blog</h1>
+    </>
+  )
+}
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  )
+}
+
+function App() {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary navigation-bar">
-        <Container>
-          <Navbar.Brand href="#home">Erik Shaver</Navbar.Brand>
+        <div className="container-fluid">
+          <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+            <img
+            src={Logo}
+            alt="Logo"
+            className="logo"
+            />
+            <span>Erik Shaver</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link as={Link} to="/" className="nav-link">About</Nav.Link>
+              <Nav.Link as={Link} to="/projects" className="nav-link">Projects</Nav.Link>
+              <Nav.Link as={Link} to="/blog" className="nav-link">Blog</Nav.Link>
               <NavDropdown title="Resources" id="basic-nav-dropdown">
                 <NavDropdown.Item href="https://github.com/ki-krpto">
                   Github
                 </NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link as={Link} to="/contact" className="nav-link">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        </Container>
+        </div>
       </Navbar>
+      <Routes>
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/" element={<About/>} />
+      </Routes>
     </>
   );
 }
 
-export default BasicExample;
+export default App;
